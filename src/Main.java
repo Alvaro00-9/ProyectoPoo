078de4ccfa20f0f59489694be423530340e6dcfd
Alvaro00-9 <alvdiaz25@alumnos.utalca.cl>
2026-05-13 11:58:53 -0400

    Commit 1, primer párrafo del proyecto, cambis minimos

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
-public InventarioLetras {
+public class InventarioLetras {
     private int [ ] Letras; // letras almacenadas
     private int totalcnt; //cantidad de letras cuyo conteo general resulta distinto de 0
     private int conteovacio;
 
+
+    public InventarioLetras(String datos) {
+        letras = new int [27];
+        totalcnt = 0;
+        conteovacio = 0;
+    }
 }
