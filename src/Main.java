a5dc8561f4e70ea3983105490d72dcd9a8e0fd0f
Alvaro00-9 <alvdiaz25@alumnos.utalca.cl>
2026-05-13 11:37:04 -0400

    Commit 1, inicio de proyecto

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,6 @@
-//Hello
+public InventarioLetras {
+    private int [ ] Letras; // letras almacenadas
+    private int totalcnt; //cantidad de letras cuyo conteo general resulta distinto de 0
+    private int conteovacio;
+
+}
