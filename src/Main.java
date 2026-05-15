f34bfa099e77c48d181e56935fe8dcec0a92e2a2
Alvaro00-9 <alvdiaz25@alumnos.utalca.cl>
2026-05-15 12:56:20 -0400

    Commit 4, Debería ser el 3 pero no estoy seguro, no revisé cuantos llevo, cambié la mayoría del código porque me di cuenta de que no cumplía lo que pedía el PDF, cmo los nombres de las clases, que creía que eran ejemplos.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,38 @@
 public class InventarioLetras {
-    private int [ ] Letras; // letras almacenadas
-    private int totalcnt; //cantidad de letras cuyo conteo general resulta distinto de 0
-    private int conteovacio;
+    private int  [] letras;
+    private int totalCount;
+    private int nonZeroCount; //cambio de nombre debido a que revisé mejor el PDF y creí
+    //que los nombres een el eran meros ejemplos.
 
 
-    public InventarioLetras(String datos) {
-        letras = new int [27];
-        totalcnt = 0;
-        conteovacio = 0;
+    public InventarioLetras(String data) {
+        letras = new int [26];
+        totalCount = 0;
+        nonZeroCount = 0;
 
-        for (int i = 0; i < datos.length(); i++) {
-            char c = Character.toLowerCase(datos.charAt(i));
+
+        for (int i = 0; i < data.length(); i++) {
+            char c = Character.toLowerCase(data.charAt(i));
+
+            if (c >= 'A' && c <= 'Z') {
+                int indice = c - 'a';
+
+                //aumenta el conteo partiendo desde 0 || La letra aparece por primera vez
+
+                if(letras[indice] == 0) {
+                    nonZeroCount++;
+                }
+
+                letras[indice]++;
+                totalCount++;
+            }
+        }
+    }
+    //Cifrado Cesár
+    public char encriptadoCesar(char letra) {
+        letra = Character.toLowerCase(letra);
+        if(letra < 'a' || letra > 'z') {
+            throw new IllegalArgumentException("Caracter no válido");
         }
     }
 }
