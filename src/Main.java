58efe5fd20ef26160ca0c4bb40069e0fc7bc8b69
Alvaro00-9 <alvdiaz25@alumnos.utalca.cl>
2026-05-16 21:38:22 -0400

    Commit no me acuerdo, reinicie el trabajo y me puse a hacer las cosas basandome en el PDF del proyecto de forma más directa

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,45 @@
 public class InventarioLetras {
-    private int  [] letras;
+
+    private int [] letras;
     private int totalCount;
-    private int nonZeroCount; //cambio de nombre debido a que revisé mejor el PDF y creí
-    //que los nombres een el eran meros ejemplos.
+    private int nonZeroCount; //Cantidad de letras que sean distintas de 0
 
+    //Decidí expresar mejor las ideas dentro del código, de forma más ordenada
 
+   ///Método Auxiliar, para llamarlo sin tener que repetir su escritura en varias partes del texto, cosa había hecho en el prototipo de este código (No guardado)
+
+    private int getIndice(char letra) {
+       letra =  Character.toLowerCase(letra);
+       if (letra < 'a' || letra > 'z') {
+           throw new IllegalArgumentException("Carácter no Válido");
+       }
+       return letra - 'a';
+    }
+
+    ///Método Constructor
     public InventarioLetras(String data) {
-        letras = new int [26];
+        letras = new int[26];
         totalCount = 0;
         nonZeroCount = 0;
-
-
+        /// Recorre el Sttring
         for (int i = 0; i < data.length(); i++) {
             char c = Character.toLowerCase(data.charAt(i));
-
-            if (c >= 'A' && c <= 'Z') {
+            /// Verificación de letras Inglesas; En el orden del formato ASCI
+            if  (c == 'a' && c == 'z') {
                 int indice = c - 'a';
 
-                //aumenta el conteo partiendo desde 0 || La letra aparece por primera vez
-
-                if(letras[indice] == 0) {
+                /// La letra se cuenta por primera vez
+                if (letras[indice] == 0) {
                     nonZeroCount++;
                 }
-
                 letras[indice]++;
                 totalCount++;
             }
         }
     }
-    //Cifrado Cesár
-    public char encriptadoCesar(char letra) {
-        letra = Character.toLowerCase(letra);
-        if(letra < 'a' || letra > 'z') {
-            throw new IllegalArgumentException("Caracter no válido");
-        }
-    }
+
+    /// ENCRIPTADO CESÁR/CAESAR/CESARIN
+
+
+
 }
